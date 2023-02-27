9febc4c839b3307c83cec6dbf0dc6c4fd3934a63
Stefan Shklifov <s.shklifov@gmail.com>
2023-02-27 09:44:44 +0200

    Uniforms and triangle rotation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cmath>
 
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
@@ -40,16 +41,21 @@ int main() {
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, resizeCallback);
 
+	// rad = deg / 180 * pi
+	// deg = rad / pi * 180
+
 	const char* vertexShaderSource =
 		"#version 330\n"
-		"layout (location = 0) in vec3 aPos;\n"
+		"layout (location = 0) in vec2 aPos;\n"
 		"layout (location = 1) in vec3 aColor;\n"
+		"uniform float t;\n"
 		"out vec3 vertexShaderColor;\n"
 		"void main() {\n"
-		"	vec4 t = vec4(-0.5, 0.0, 0.0, 0.0);\n"
-		"	vec4 res = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"	gl_Position = res + t;\n"
-		"	\n"
+		"	mat2 rot;\n"
+		"	float angle = t * 3.1415 * 2;\n"
+		"	rot[0] = vec2(cos(angle), sin(angle));\n"
+		"	rot[1] = vec2(-sin(angle), cos(angle));\n"
+		"	gl_Position = vec4(rot * aPos, 0.0, 1.0);\n"
 		"	vertexShaderColor = aColor;\n"
 		"}\n";
 	unsigned vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -104,36 +110,62 @@ int main() {
 	glBindVertexArray(vao);
 
 	float points[] = {
-		-0.5f, -0.5f, 0.f, // point1
+		-0.5f, -0.5f,      // point1
 		1.f, 0.f, 0.f,	   // color1
-		0.5f, -0.5f, 0.f,  // point2
-		1.f, 0.f, 0.f,	   // color2
-		0.0f,  0.5f, 0.f,  // point3
-		1.f, 0.f, 0.f,	   // color3
+		0.5f, -0.5f,       // point2
+		0.f, 1.f, 0.f,	   // color2
+		0.0f,  0.5f,       // point3
+		0.f, 0.f, 1.f,	   // color3
 		// Second triangle
-		-0.5f, 0.5f, 1.f,	 // point1
-		0.f, 1.f, 0.f,		 // color1
-		0.0f, 0.0f, 1.f,	 // point2
-		0.f, 1.f, 0.f,		 // color2
-		-0.8f,	-0.5f, 1.f,  // point3
-		0.f, 1.f, 0.f,		 // color3
+		-0.5f, 0.5f,	     // point1
+		1.f, 1.f, 0.f,		 // color1
+		0.0f, 0.0f,	         // point2
+		0.f, 1.f, 1.f,		 // color2
+		-0.8f,	-0.5f,       // point3
+		1.f, 0.f, 1.f,		 // color3
 	};
+
+#if 0
+	// Texture loading. TODO
+	unsigned tex;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE, tex);
+	// 600 x 800 -> 480000
+	// 800 x 600 -> 480000
+	// glTexImage2D(GL_TEXTURE, )
+
+	FILE * fp = fopen("/home/stef/Downloads/brick.rgb", "r");
+	float p1[3];
+	fread(p1, sizeof(float), 3, fp);
+
+	float pixelArray[120];
+	fread(pixelArray, sizeof(float), 120, fp);
+
+	fclose(fp);
+#endif
 
 	unsigned vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	/* glEnable(GL_DEPTH_TEST); */
+	int uniformLocation = glGetUniformLocation(program, "t");
+	glUniform1f(uniformLocation, 0.5f);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(1.f, 1.f, 1.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwPollEvents();
+
+		float now = (float)glfwGetTime();
+		double unused = 0.f;
+		float t = (float)modf(now / 10.f, &unused);
+		// printf("The time is: %f. Which leads to t=%f\n", now, t);
+		glUniform1f(uniformLocation, t);
 
 		glUseProgram(program);
 		glBindVertexArray(vao);
