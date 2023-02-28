dd84efc8647b1fe078d070ebd28c85f4e4d6d3b4
Stefan Shklifov <s.shklifov@gmail.com>
2023-02-28 22:21:32 +0200

    Lecture 4
    Render texture

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <cmath>
+#include <cstring>
+#include <cassert>
 
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
@@ -47,16 +49,11 @@ int main() {
 	const char* vertexShaderSource =
 		"#version 330\n"
 		"layout (location = 0) in vec2 aPos;\n"
-		"layout (location = 1) in vec3 aColor;\n"
-		"uniform float t;\n"
-		"out vec3 vertexShaderColor;\n"
+		"layout (location = 1) in vec2 aTextureCoords;\n"
+		"out vec2 st;\n"
 		"void main() {\n"
-		"	mat2 rot;\n"
-		"	float angle = t * 3.1415 * 2;\n"
-		"	rot[0] = vec2(cos(angle), sin(angle));\n"
-		"	rot[1] = vec2(-sin(angle), cos(angle));\n"
-		"	gl_Position = vec4(rot * aPos, 0.0, 1.0);\n"
-		"	vertexShaderColor = aColor;\n"
+		"	st = aTextureCoords;\n"
+		"	gl_Position = vec4(aPos, 0.0, 1.0);\n"
 		"}\n";
 	unsigned vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -73,10 +70,12 @@ int main() {
 
 	const char* fragmentShaderSource =
 		"#version 330\n"
-		"in vec3 vertexShaderColor;\n"
+		"in vec2 st;\n"
 		"out vec4 FragColor;\n"
+		"uniform sampler2D tex;"
 		"void main() {\n"
-		"	FragColor = vec4(vertexShaderColor, 1.0);\n"
+		"	FragColor = texture(tex, st);\n"
+		"	//FragColor = vec4(st, 0.0, 1.0);\n"
 		"}\n";
 	unsigned fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -111,66 +110,72 @@ int main() {
 
 	float points[] = {
 		-0.5f, -0.5f,      // point1
-		1.f, 0.f, 0.f,	   // color1
+		0.f, 0.f,          // uv1
 		0.5f, -0.5f,       // point2
-		0.f, 1.f, 0.f,	   // color2
+		1.f, 0.f,          // uv2
 		0.0f,  0.5f,       // point3
-		0.f, 0.f, 1.f,	   // color3
-		// Second triangle
-		-0.5f, 0.5f,	     // point1
-		1.f, 1.f, 0.f,		 // color1
-		0.0f, 0.0f,	         // point2
-		0.f, 1.f, 1.f,		 // color2
-		-0.8f,	-0.5f,       // point3
-		1.f, 0.f, 1.f,		 // color3
+		0.5f, 1.f,          // uv3
 	};
-
-#if 0
-	// Texture loading. TODO
-	unsigned tex;
-	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE, tex);
-	// 600 x 800 -> 480000
-	// 800 x 600 -> 480000
-	// glTexImage2D(GL_TEXTURE, )
-
-	FILE * fp = fopen("/home/stef/Downloads/brick.rgb", "r");
-	float p1[3];
-	fread(p1, sizeof(float), 3, fp);
-
-	float pixelArray[120];
-	fread(pixelArray, sizeof(float), 120, fp);
-
-	fclose(fp);
-#endif
 
 	unsigned vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	int uniformLocation = glGetUniformLocation(program, "t");
-	glUniform1f(uniformLocation, 0.5f);
+	const int width = 1000;
+	const int height = 663;
+	size_t numPixels = width * height;
+	size_t imageSize = numPixels * 3 * sizeof(unsigned char);
+	unsigned char* imageData = static_cast<unsigned char*>(malloc(imageSize));
+
+	FILE * fp = fopen("/home/stef/Downloads/brick.rgb", "r");
+	size_t numRead = fread(imageData, 1, imageSize, fp);
+	assert(!ferror(fp));
+	assert(numRead == numPixels*3);
+	assert(!feof(fp));
+	int c = fgetc(fp);
+	assert(c == EOF);
+	assert(feof(fp));
+
+	fclose(fp);
+
+#if 0
+	for (size_t i = 0; i < imageSize; i += 1) {
+		if (i % 3 == 0) {
+			imageData[i] = 255;
+		} else {
+			imageData[i] = 0;
+		}
+	}
+#endif
+
+	unsigned tex;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,  GL_RGB, GL_UNSIGNED_BYTE, imageData); // TODO try other internal format
+	glGenerateMipmap(GL_TEXTURE_2D);
+	free(imageData);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(1.f, 1.f, 1.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwPollEvents();
 
-		float now = (float)glfwGetTime();
-		double unused = 0.f;
-		float t = (float)modf(now / 10.f, &unused);
-		// printf("The time is: %f. Which leads to t=%f\n", now, t);
-		glUniform1f(uniformLocation, t);
-
 		glUseProgram(program);
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindTexture(GL_TEXTURE_2D, tex);
 
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 	}
 
