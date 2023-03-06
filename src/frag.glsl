b8a8f51d9717cd1a8ec1cd531d85388e9856810a
Stefan Shklifov <s.shklifov@gmail.com>
2023-03-06 10:21:27 +0200

    Refactor shaders after adding syntax highlight

--- src/frag.glsl
+++ src/frag.glsl
@@ -1,11 +1,16 @@
 #version 330
-in vec2 st;
-out vec4 FragColor;
+
 uniform sampler2D tex0;
 uniform sampler2D tex1;
+
 in vec3 vertexPos;
+in vec2 textureCoords;
+in vec3 normal;
+
+out vec4 FragColor;
+
 void main() {
-	vec3 diffuseColor = vec3(texture(tex0, st));
+	vec3 diffuseColor = vec3(texture(tex0, textureCoords));
 	float ambientLight = 0.1f;
 	vec3 ambientColor = diffuseColor * ambientLight;
 	FragColor = vec4(ambientColor, 1.0);
--- src/vert.glsl
+++ src/vert.glsl
@@ -1,14 +1,20 @@
 #version 330
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTextureCoords;
-layout (location = 2) in vec3 aNormal;
+
+layout (location = 0) in vec3 inPos;
+layout (location = 1) in vec2 inTextureCoords;
+layout (location = 2) in vec3 inNormal;
+
 uniform mat4 proj;
 uniform mat4 model;
 uniform mat4 view;
-out vec2 st;
+
 out vec3 vertexPos;
+out vec2 textureCoords;
+out vec3 normal;
+
 void main() {
-	vertexPos = vec3(model * vec4(aPos, 1.0));
-	gl_Position = proj * view * model * vec4(aPos, 1.0);
-	st = aTextureCoords;
+	vertexPos = vec3(model * vec4(inPos, 1.0));
+	textureCoords = inTextureCoords;
+	normal = inNormal;
+	gl_Position = proj * view * model * vec4(inPos, 1.0);
 }
